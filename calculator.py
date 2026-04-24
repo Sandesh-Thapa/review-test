f0935ea53010cba64d179ff769b5e279caf31005
Sandesh Thapa <sandeshthapa426@gmail.com>
2026-04-24 11:44:01 +0545

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def sum_two_number(first_num, num_second):
+    a = first_num
+    b = num_second
+    return first_num + num_second
+
+x = 10
+#
+if __name__ == "__main__":
+    print("Sum of")
+    print("Two numbers: ", sum_two_number(10, "TEST"))
